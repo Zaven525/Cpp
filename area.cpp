624b7e1401f25bce1ae4cfd67b92c7bff74a667d
Zaven525 <71457724+Zaven525@users.noreply.github.com>
2025-02-11 19:48:32 +0400

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,15 @@
+double area(double radius){
+    return radius * radius * 3.14;
+}
+
+int area(double width, double height, char r){
+    return width*height;
+}
+
+int area(double width, double height){
+    return width*height/2;
+}
+
+int area(int side){
+    return side * side;
+}
